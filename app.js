fbd5b8cd6e06be79d3c07561719c94e2d325ac42
Matt Richards <matt@lucidmoon.co.uk>
2015-08-28 14:28:19 +0200

    Chore: initial import

---
+++ app.js
@@ -0,0 +1,7 @@
+import 'zone.js'
+import 'reflect-metadata'
+import 'es6-shim'
+import { bootstrap } from 'angular2/angular2'
+import MyAppComponent from './app/components/MyApp'
+
+bootstrap(MyAppComponent)
---
+++ app/components/MyApp.js
@@ -0,0 +1,15 @@
+import { Component, View } from 'angular2/angular2'
+
+@Component({
+  selector: 'my-app'
+})
+@View({
+  template: '<h1>Hello {{ name }}</h1>'
+})
+
+export default class MyAppComponent {
+  name: string
+  constructor() {
+    this.name = 'Alice'
+  }
+}
